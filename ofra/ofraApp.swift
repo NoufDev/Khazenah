4c0a6a71347b7cb56462cb0690d6215c2d1fc3c9
Nouf <nouf.sul@outlook.com>
2023-01-12 11:11:05 +0300

    Initial Commit

---
+++ ofra/ofraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ofraApp.swift
+//  ofra
+//
+//  Created by nouf on 19/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ofraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
